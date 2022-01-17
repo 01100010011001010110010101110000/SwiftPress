d12f918c05afc50472fb09df13ea6fa5f8887584
Tyler Gregory <tdgregory@protonmail.com>
2022-01-17 17:08:56 -0600

    Add Apple's MD package as a dependency

--- Package.swift
+++ Package.swift
@@ -6,23 +6,22 @@ import PackageDescription
 let package = Package(
     name: "SwiftPress",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftPress",
-            targets: ["SwiftPress"]),
+            targets: ["SwiftPress"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftPress",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SwiftPressTests",
-            dependencies: ["SwiftPress"]),
+            dependencies: ["SwiftPress"]
+        ),
     ]
 )
