6473618d5b7a1fba05b9913a87255626aeedfbb0
Tyler Gregory <tdgregory@protonmail.com>
2022-01-17 17:12:54 -0600

    Add swiftformat and version files, run formatter

--- Package.swift
+++ Package.swift
@@ -4,24 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftPress",
-    products: [
-        .library(
-            name: "SwiftPress",
-            targets: ["SwiftPress"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
-    ],
-    targets: [
-        .target(
-            name: "SwiftPress",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "SwiftPressTests",
-            dependencies: ["SwiftPress"]
-        ),
-    ]
+  name: "SwiftPress",
+  products: [
+    .library(
+      name: "SwiftPress",
+      targets: ["SwiftPress"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
+  ],
+  targets: [
+    .target(
+      name: "SwiftPress",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "SwiftPressTests",
+      dependencies: ["SwiftPress"]
+    ),
+  ]
 )
